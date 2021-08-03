ac271903cdb9cc02a35a1f7ec82d21c4c0cbcf98
Gabryjel <ziomus958@gmail.com>
2021-08-03 12:32:04 +0200

    test

---
+++ d4/Start.js
@@ -0,0 +1,14 @@
+let tab = [1,3,5,8,20,4,100];
+let sum = 0;
+for(let i = 0; i<tab.length; i++){
+
+  sum = (tab[i] + sum)
+  
+ console.log(sum)
+}
+console.log(sum/tab.length);
+console.log(sum*tab.length);
+
+
+
+
